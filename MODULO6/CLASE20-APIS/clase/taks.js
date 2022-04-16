01228dedfc5fa0ad4316c2f02e62246ba8e79c1d
LuisF-1995 <luis-1995@live.com.ar>
2022-04-16 15:40:29 -0500

    cargue al 16-abril-2022

---
+++ MODULO6/CLASE20-APIS/clase/taks.js
@@ -0,0 +1,128 @@
+// SEGURIDAD: Si no se encuentra en localStorage info del usuario
+// no lo deja acceder a la página, redirigiendo al login inmediatamente.
+if (!localStorage.jwt) {
+  location.replace('./index.html');
+}
+
+/* ------ comienzan las funcionalidades una vez que carga el documento ------ */
+window.addEventListener('load', function () {
+
+  /* ---------------- variables globales y llamado a funciones ---------------- */
+ 
+  const urlTareas = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
+  const urlUsuario = 'https://ctd-todo-api.herokuapp.com/v1/users/getMe';
+  const token = JSON.parse(localStorage.jwt);
+
+
+  const formCrearTarea = document.querySelector('.nueva-tarea');
+  const nuevaTarea = document.querySelector('#nuevaTarea');
+  const btnCerrarSesion = document.querySelector('#closeApp');
+  
+  obtenerNombreUsuario();
+  consultarTareas();
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                          FUNCIÓN 1 - Cerrar sesión                         */
+  /* -------------------------------------------------------------------------- */
+
+  btnCerrarSesion.addEventListener('click', function () {
+    const cerrarSesion = confirm("¿Desea cerrar sesión?");
+    if (cerrarSesion) {
+      //limpiamos el localstorage y redireccioamos a login
+      localStorage.clear();
+      location.replace('./index.html');
+    }
+   
+
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                 FUNCIÓN 2 - Obtener nombre de usuario [GET]                */
+  /* -------------------------------------------------------------------------- */
+
+  function obtenerNombreUsuario() {
+    const settings = {
+      method: 'GET',
+      headers: {
+        authorization: token
+      }
+    };
+    console.log("Consultando mi usuario...");
+    fetch(urlUsuario, settings)
+    .then(response => response.json())
+    .then(data => {
+      console.log("Nombre de usuario:");
+      console.log(data.firstName);
+      const nombreUsuario = document.querySelector('.user-info p');
+      nombreUsuario.innerText = data.firstName;
+    })
+
+  };
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                 FUNCIÓN 3 - Obtener listado de tareas [GET]                */
+  /* -------------------------------------------------------------------------- */
+
+  function consultarTareas() {
+    
+    
+
+    renderizarTareas(tareas);
+    botonesCambioEstado();
+    botonBorrarTarea();
+
+  };
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                    FUNCIÓN 4 - Crear nueva tarea [POST]                    */
+  /* -------------------------------------------------------------------------- */
+
+  formCrearTarea.addEventListener('submit', function (event) {
+    
+
+
+
+
+  });
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                  FUNCIÓN 5 - Renderizar tareas en pantalla                 */
+  /* -------------------------------------------------------------------------- */
+  function renderizarTareas(listado) {
+
+
+
+
+
+
+
+  };
+
+  /* -------------------------------------------------------------------------- */
+  /*                  FUNCIÓN 6 - Cambiar estado de tarea [PUT]                 */
+  /* -------------------------------------------------------------------------- */
+  function botonesCambioEstado() {
+    
+    
+
+
+
+  }
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                     FUNCIÓN 7 - Eliminar tarea [DELETE]                    */
+  /* -------------------------------------------------------------------------- */
+  function botonBorrarTarea() {
+   
+    
+
+    
+
+  };
+
+});
